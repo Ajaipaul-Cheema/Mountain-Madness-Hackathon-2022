42f010b07bbdc4d7a8f80238dbcc328da9960a25
Ajaipaul Cheema <ajaipaul_cheema@hotmail.com>
2022-04-30 00:10:24 -0700

    adding stock info

--- src/functions.js
+++ src/functions.js
@@ -1,31 +1,38 @@
-var buyingPower = 10000
-var portfolioValue = buyingPower 
-const SETMAX = 2.5
+var buyingPower = 10000;
+var portfolioValue = buyingPower;
+const SETMAX = 2.5;
+var purchasedProducts = [];
 
 const changeInStockPrice = (stockPrice) => {
+  // Done Through Percentage Change
+  var changeInPrice = ((Math.random() * SETMAX) / 100) * stockPrice;
 
-    // Done Through Percentage Change 
-    var changeInPrice = ((Math.random() * (SETMAX)) / 100) * stockPrice
+  if (Math.floor(Math.random() * 10) >= 5) {
+    stockPrice = stockPrice + changeInPrice;
+    portfolioValue += changeInPrice;
+  } else {
+    if (stockPrice < 0) return;
+    stockPrice = stockPrice - changeInPrice;
+    portfolioValue -= changeInPrice;
+  }
 
-    if(Math.floor(Math.random() * 10) >= 5) {
-        stockPrice = stockPrice + changeInPrice
-        portfolioValue += changeInPrice
-    } else {
-        if(stockPrice < 0) return 
-        stockPrice = stockPrice - changeInPrice
-        portfolioValue -= changeInPrice
-    }
-
-    return stockPrice
-}
+  return stockPrice;
+};
 
 const buyingStock = (stockPrice) => {
-    if(buyingPower < 0) return 
-    buyingPower -= stockPrice;
-    portfolioValue -= stockPrice;
-}
+  if (buyingPower < 0) return;
+  buyingPower -= stockPrice;
+  portfolioValue -= stockPrice;
+};
 
 const sellingStock = (stockPrice) => {
-    buyingPower += stockPrice
-    portfolioValue += stockPrice
-}
+  buyingPower += stockPrice;
+  portfolioValue += stockPrice;
+};
+
+const addStockInfo = (name, price) => {
+  purchasedProducts.push({
+    key:   name,
+    value: price
+});
+};
