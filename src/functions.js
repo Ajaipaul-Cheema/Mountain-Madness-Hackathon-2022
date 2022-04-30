8df43a8f86dd18806a05c6fb3ddd1b308835149b
Haris Ahmad <HarisAhmad04@hotmail.com>
2022-04-30 01:30:29 -0700

    percentage

--- src/functions.js
+++ src/functions.js
@@ -1,34 +1,37 @@
-var buyingPower = 10000;
-var portfolioValue = buyingPower;
-const SETMAX = 2.5;
-var purchasedProducts = [];
+var buyingPower = 10000
+var portfolioValue = buyingPower
+var percentChange = 0
+const SETMAX = 2.5
+var purchasedProducts = []
 
 const changeInStockPrice = (stockPrice) => {
   // Done Through Percentage Change
-  var changeInPrice = ((Math.random() * SETMAX) / 100) * stockPrice;
+  percentChange = (Math.random() * SETMAX)
+  var changeInPrice = (percentChange / 100) * stockPrice
 
   if (Math.floor(Math.random() * 10) >= 5) {
-    stockPrice = stockPrice + changeInPrice;
-    portfolioValue += changeInPrice;
+    stockPrice = stockPrice + changeInPrice
+    portfolioValue += changeInPrice
   } else {
     if (stockPrice < 0) return;
-    stockPrice = stockPrice - changeInPrice;
-    portfolioValue -= changeInPrice;
+    stockPrice = stockPrice - changeInPrice
+    portfolioValue -= changeInPrice
   }
 
   return stockPrice;
-};
+}
 
 const buyingStock = (stockPrice) => {
-  if (buyingPower < 0) return;
-  buyingPower -= stockPrice;
-  portfolioValue -= stockPrice;
-};
+  if (buyingPower < 0) return
+  buyingPower -= stockPrice
+  portfolioValue -= stockPrice
+}
 
 const sellingStock = (stockPrice) => {
-  buyingPower += stockPrice;
-  portfolioValue += stockPrice;
-};
+  if(stockPrice < 0) return
+  buyingPower += stockPrice
+  portfolioValue += stockPrice
+}
 
 const addStockInfo = (name, price) => {
     purchasedProducts.push({
